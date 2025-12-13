4720f490f28053ceac2d5bd668b793c2d305cf08
AnnWann <uanabgasparim@gmail.com>
2025-12-13 15:41:51 -0300

    fix import issue in vercel

--- api/ical/cron.ts
+++ api/ical/cron.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { runImport } from './importWorker';
+import { runImport } from './importWorker.js';
 
 export default async function handler(_req: VercelRequest, res: VercelResponse) {
   try {
--- api/ical/export.ts
+++ api/ical/export.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import supabaseAdmin from '../../src/util/supabaseClient';
-import { generateICS } from '../../src/util/ical';
+import supabaseAdmin from '../../src/util/supabaseClient.js';
+import { generateICS } from '../../src/util/ical.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
